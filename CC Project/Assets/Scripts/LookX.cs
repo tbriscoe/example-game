7e94aa3b3e2983f1c0618bfb70aaf887593df3cd
Anthony Briscoe <tony.briscoe1@gmail.com>
2018-08-29 21:04:09 +0100

    more changes

---
+++ CC Project/Assets/Scripts/LookX.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookX : MonoBehaviour {
+
+	[SerializeField] private float _sensitivity = 1.0f;
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float _mouseX = Input.GetAxis("Mouse X");
+
+		// Get my current rotation and assign it a new rotation
+
+		// transform.rotation uses Quaternions which is a handful of methods to help us with rotations
+		
+		// To change the individual angle
+
+		Vector3 newRotation = transform.localEulerAngles;
+		newRotation.y += _mouseX * _sensitivity;
+		transform.localEulerAngles = newRotation;
+		// transform.localEulerAngles = new Vector3(newRotation.x, newRotation.y, newRotation.z);
+	}
+}
---
+++ CC Project/Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	private CharacterController _contoller;
+	[SerializeField] private float _speed = 3.5f;
+	private float _gravity = 9.81f;
+	// Use this for initialization
+	void Start () 
+	{
+		_contoller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	 CalculateMovement();
+	}
+
+	void CalculateMovement()
+	{
+		float horizontalInput = Input.GetAxis("Horizontal");
+		float verticalInput = Input.GetAxis("Vertical");
+		Vector3 direction = new Vector3(horizontalInput, 0, verticalInput);
+		Vector3 velocity = direction * _speed;
+		velocity.y -= _gravity;
+		velocity = transform.TransformDirection(velocity); // reassign world space values
+		_contoller.Move(velocity * Time.deltaTime); // this moves us in world space
+		// our players coordinate system is getting passed in as local space
+	}
+}
